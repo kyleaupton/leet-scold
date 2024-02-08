ace60a549f3af43e1307c704bc859fd398455815
Kyle Upton <kyleaupton@gmail.com>
2024-02-07 21:10:14 -0500

    added discord client

---
+++ src/discord.ts
@@ -0,0 +1,33 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { logger } from './logger.js'
+import 'dotenv/config'
+
+let client: Client | undefined
+
+export const initializeDiscord = async (): Promise<Client> => {
+  const requiredEnv = ['DISCORD_BOT_ID', 'DISCORD_BOT_KEY', 'DISCORD_BOT_GUILD', 'DISCORD_BOT_CHANNEL']
+  for (const env of requiredEnv) {
+    if (!process.env[env]) {
+      throw new Error(`${env} is not set`)
+    }
+  }
+
+  await new Promise<void>((resolve) => {
+    client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+    client.once(Events.ClientReady, (readyClient) => {
+      logger.info(`Discord client logged in as ${readyClient.user.tag}`)
+      resolve()
+    })
+
+    void client.login(process.env.DISCORD_BOT_KEY)
+  })
+
+  if (!client) {
+    throw new Error('Client not initialized')
+  }
+
+  return client
+}
+
+export { client }
