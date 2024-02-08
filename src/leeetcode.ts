4b4efc9030e791c8a46d221cbc796f128f536645
0xc0le <cole511@protonmail.com>
2024-02-08 18:33:24 -0500

    added daily problem for GPT

--- src/leeetcode.ts
+++ src/leeetcode.ts
@@ -28,3 +28,29 @@ export const wasNotACompletePieceOfShitToday = async (username: string): Promise
 
   return (submissions[today] ?? 0) > 0
 }
+
+export const fetchDailyProblem = async (): Promise<string> => {
+  const linkRes = await fetch('https://leetcode.com/graphql/', {
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: '{"query":"\\n    query questionOfToday {\\n  activeDailyCodingChallengeQuestion {\\n    date\\n    userStatus\\n    link\\n    question {\\n      acRate\\n      difficulty\\n      freqBar\\n      frontendQuestionId: questionFrontendId\\n      isFavor\\n      paidOnly: isPaidOnly\\n      status\\n      title\\n      titleSlug\\n      hasVideoSolution\\n      hasSolution\\n      topicTags {\\n        name\\n        id\\n        slug\\n      }\\n    }\\n  }\\n}\\n    ","variables":{},"operationName":"questionOfToday"}',
+    method: 'POST'
+  })
+
+  const dailyLink = (await linkRes.json()).data.activeDailyCodingChallengeQuestion.link
+
+  const problemName = dailyLink.split('/')[2]
+
+  const questionRes = await fetch('https://leetcode.com/graphql/', {
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: `{"query":"\\n    query questionContent($titleSlug: String!) {\\n  question(titleSlug: $titleSlug) {\\n    content\\n    mysqlSchemas\\n    dataSchemas\\n  }\\n}\\n    ","variables":{"titleSlug":"${problemName}"},"operationName":"questionContent"}`,
+    method: 'POST'
+  })
+
+  const questionText = (await questionRes.json()).data.question.content
+
+  return questionText
+}
